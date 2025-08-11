6cd76501f3633f779b3fa7b6c37558e097fb5ad3
Paulo Nascimento <victinhopaulo64@gmail.com>
2025-08-11 09:48:59 -0300

    Commit inicial

---
+++ src/main/java/com/example/sistema_futebol/SistemaFutebolApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistema_futebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaFutebolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaFutebolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sistema_futebol/SistemaFutebolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistema_futebol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaFutebolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
